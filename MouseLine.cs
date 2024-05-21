ff9abb473dedfdf630de4e48a58b0b702377fda0
mirasl <santaluciamira+chia@gmail.com>
2024-05-21 14:09:40 -0700

    Adds method to calculate slope of brushstroke

--- MouseLine.cs
+++ MouseLine.cs
@@ -27,14 +27,39 @@ public class MouseLine : Line2D
             }
         }
 
-        if (Points.Length > 60)
+        if (Points.Length > 0 && !slicing)
         {
             RemovePoint(0);
         }
     }
 
-    public async void FinishSlice()
+    public void FinishSlice()
     {
+        GD.Print(RegressedSlope(Points));
+    }
 
+    // Calculates slope of array of points with a linear regression:
+    public float RegressedSlope(Vector2[] points)
+    {
+        // Calculate means:
+        float xMean = 0;
+        float yMean = 0;
+        foreach (Vector2 point in points)
+        {
+            xMean += point.x;
+            yMean += point.y;
+        }
+        xMean /= points.Length;
+        yMean /= points.Length;
+
+        // Calculate xi - xMean and yi - yMean
+        float sumXYDiff = 0;
+        float sumXDiffSquared = 0;
+        foreach (Vector2 point in points)
+        {
+            sumXYDiff += (point.x - xMean) * (point.y - yMean);
+            sumXDiffSquared += (point.x - xMean) * (point.x - xMean);
+        }
+        return -sumXYDiff / sumXDiffSquared;
     }
 }
