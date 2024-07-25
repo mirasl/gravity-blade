ffe254dada25c7eb49759a21f9e7edf3afebe420
mirasl <santaluciamira+chia@gmail.com>
2024-07-25 00:03:22 -0700

    Connects basic functionality of pause menu

--- Pause.cs
+++ Pause.cs
@@ -3,13 +3,29 @@ using System;
 
 public class Pause : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Signal] delegate void Restart();
+    [Signal] delegate void Quit();
 
-    // Called when the node enters the scene tree for the first time.
+    [Export] Vector2 circleSelectPosition1 = new Vector2();
+    [Export] Vector2 circleSelectPosition2 = new Vector2();
+    [Export] Vector2 circleSelectPosition3 = new Vector2();
+
+    enum Selection
+    {
+        Resume,
+        Restart,
+        Quit
+    }
+    Selection currentSelection = Selection.Resume;
+
+    protected AnimatedSprite circleSelect;
+
+
     public override void _Ready()
     {
+        circleSelect = GetNode<AnimatedSprite>("CircleSelect");
+        circleSelect.Position = circleSelectPosition1;
+
         Hide();
     }
 
@@ -17,7 +33,57 @@ public class Pause : Control
     {
         if (Input.IsActionJustPressed("pause"))
         {
-            GetTree().Paused = !GetTree().Paused;
+            if (!GetTree().Paused)
+            {
+                GetTree().Paused = true;
+                Show();
+                circleSelect.Position = circleSelectPosition1;
+                currentSelection = Selection.Resume;
+            }
+        }
+
+        if (Input.IsActionJustPressed("up"))
+        {
+            if (currentSelection == Selection.Restart)
+            {
+                circleSelect.Position = circleSelectPosition1;
+                currentSelection = Selection.Resume;
+            }
+            else if (currentSelection == Selection.Quit)
+            {
+                circleSelect.Position = circleSelectPosition2;
+                currentSelection = Selection.Restart;
+            }
+        }
+        if (Input.IsActionJustPressed("down"))
+        {
+            if (currentSelection == Selection.Resume)
+            {
+                circleSelect.Position = circleSelectPosition2;
+                currentSelection = Selection.Restart;
+            }
+            else if (currentSelection == Selection.Restart)
+            {
+                circleSelect.Position = circleSelectPosition3;
+                currentSelection = Selection.Quit;
+            }
+        }
+
+        if (Input.IsActionJustPressed("ui_accept") && GetTree().Paused)
+        {
+            GetTree().Paused = false;
+            if (currentSelection == Selection.Resume)
+            {
+                Hide();
+            }
+            else if (currentSelection == Selection.Restart)
+            {
+                EmitSignal("Restart");
+            }
+            else if (currentSelection == Selection.Quit)
+            {
+                EmitSignal("Quit");
+            }
         }
     }
 }
